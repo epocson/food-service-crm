1944430ff23820b321f290318bd80d9552e33a8f
Студент KSI <student@ksi.esstu.ru>
2022-10-27 17:50:27 +0800

    27.10. user.js, default.js

--- controllers/default.js
+++ controllers/default.js
@@ -1,11 +1,15 @@
- //"my first atttempt at coding"//
  exports.install = function() {
     ROUTE('+GET /*'                     );
-    ROUTE('GET /login',      view_login);   
+    ROUTE('GET /login',       view_login);   
+    ROUTE('GET  /logout',         logout);       
 }
 
-function view_login() {
-    var self = this;   
-    self.sm = F.sitemap('auth');                      
+function view_login() {                      
     self.view('page/login'); 
+}
+function logout() {
+    var self = this;        
+    MAIN.session.remove(self.sessionid);    
+    self.cookie(CONF.cookie, '', '-1 day');                
+    self.redirect('/');	
 }
